25aca33c6b331a984001e6f5d8f0765c2666b8f3
Matheus Albert <54319284+Albert1616@users.noreply.github.com>
2024-02-06 14:02:11 -0300

    fix:refatoração de de variável env

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -18,7 +18,6 @@ export async function createUser(user: CreateUserParams) {
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
     handleError(error)
-    console.log('Connection error')
   }
 }
 
